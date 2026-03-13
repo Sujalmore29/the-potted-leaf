5d495bc3a14235663e7f721169f0c7f5dc881a8f
Sujalmore29 <moresujal2912@gmail.com>
2026-03-14 01:59:27 +0530

    :feat: add main application entry point

---
+++ src/main/java/com/pottedleaf/ThePottedLeafApplication.java
@@ -0,0 +1,18 @@
+package com.pottedleaf;
+
+import me.paulschwarz.springdotenv.DotenvConfig;
+import me.paulschwarz.springdotenv.DotenvConfigLoader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePottedLeafApplication {
+
+	public static void main(String[] args) {
+
+
+
+		SpringApplication.run(ThePottedLeafApplication.class, args);
+	}
+
+}
