e2d141b11622290d27ebe3da56bc104f87568b9a
Sujalmore29 <moresujal2912@gmail.com>
2026-03-17 04:00:47 +0530

    chore: edited folder structure

--- the-potted-leaf-frontend/src/pages/Cancel.jsx
+++
@@ -1,9 +0,0 @@
-export default function Cancel() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl text-red-500">
-        Payment Cancelled
-      </h1>
-    </div>
-  );
-}
--- the-potted-leaf-frontend/src/pages/Success.jsx
+++
@@ -1,9 +0,0 @@
-export default function Success() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-green-700">
-        Payment Successful 🌿
-      </h1>
-    </div>
-  );
-}
